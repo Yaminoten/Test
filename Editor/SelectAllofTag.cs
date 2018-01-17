9632d61276c4ecabc88910a42fffffbc8e84d92d
Yaminoten <maxime.vanboquestaels@gmail.com>
2018-01-17 16:29:21 +0100

    Add
    add

---
+++ Editor/SelectAllofTag.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+class SelectAllOfTag : EditorWindow
+{
+    //[MenuItem("Window/TagSelector")]
+    public static void ShowWindow()
+    {
+        GetWindow<SelectAllOfTag>("Tag Selector");
+
+    }
+
+    public string tagName = "";
+
+    private void OnGUI()
+    {
+        tagName = EditorGUILayout.TagField(tagName,EditorStyles.textField, GUILayout.MaxWidth(100));
+
+        if (GUILayout.Button("Select"))
+        {
+            GameObject[] gos = GameObject.FindGameObjectsWithTag(tagName);
+            Selection.objects = gos;
+        }
+    }
+
+    void SelectChange()
+    {
+        
+    }
+}
---
+++ Scripts/SummonAction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(menuName = "PluggableAI/Actions/Summon")]
+public class SummonAction : Actions
+{
+
+    public override void Act(StatesController controller)
+    {
+        Summon(controller);
+    }
+
+    private void Summon(StatesController controller)
+    {
+        for (int i = 1; i < controller.enemyStats.SumNbr + 1; i++)
+            Instantiate(controller.enemyStats.SummPrefab,controller.transform.position + new Vector3(2*i , 0, 0),controller.gameObject.transform.rotation);
+    }
+
+}
